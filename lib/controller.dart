9d8dfcec4db960c02a2051ce5aaf6ac6da7c52cf
Victor <victormedeiros300@gmail.com>
2021-05-25 17:34:22 -0300

    Atividade Classe Stream

---
+++ lib/controller.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'home_model.dart';
+
+class ControllerHome {
+  HomeModel _model = HomeModel();
+
+  Future<List<int>> get lista => _model.lista;
+
+  StreamController<List<int>> streamLista = StreamController();
+
+  void loadLista() {
+    _model.loadLista();
+    _model.lista.then((value) {
+      streamLista.add(value);
+    });
+  }
+}
---
+++ lib/home_view.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:stream_mvvm_flutter/controller.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = ControllerHome();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: StreamBuilder<List<int>>(
+                  stream: controller.streamLista.stream,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState != ConnectionState.active) {
+                      return CircularProgressIndicator();
+                    }
+
+                    if (snapshot.hasData) {
+                      var array = snapshot.data ?? [];
+                      return ListView.builder(
+                        itemCount: array.length,
+                        itemBuilder: (context, index) {
+                          return Center(
+                            child: Container(
+                              margin: EdgeInsets.all(10),
+                              width: 100,
+                              height: 30,
+                              child: Center(child: Text('$index')),
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black)),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                    return CircularProgressIndicator();
+                  },
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              color: Colors.black,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    controller.loadLista();
+                  });
+                },
+                child: Text('Load'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
